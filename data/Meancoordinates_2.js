b8db811e1b77938aab8a96631e452d257760aae1
dbharris92 <harris.db92@gmail.com>
2020-11-25 11:07:14 -0500

    new coords

--- data/Meancoordinates_2.js
+++ data/Meancoordinates_2.js
@@ -3,6 +3,6 @@ var json_Meancoordinates_2 = {
 "name": "Meancoordinates_2",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "MEAN_X": -84.688577932222572, "MEAN_Y": 38.74399186857525 }, "geometry": { "type": "Point", "coordinates": [ -84.688577932222572, 38.74399186857525 ] } }
+{ "type": "Feature", "properties": { "MEAN_X": -84.312367226872226, "MEAN_Y": 38.877112551138772 }, "geometry": { "type": "Point", "coordinates": [ -84.312367226872226, 38.877112551138772 ] } }
 ]
 }
