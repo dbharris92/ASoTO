5239f6464f80df1e1e3639f10f95263ff39343a4
dbharris92 <harris.db92@gmail.com>
2020-11-25 09:45:15 -0500

    Initial commit

---
+++ qgis2web_2020_11_25-09_44_23_343864/data/Meancoordinates_2.js
@@ -0,0 +1,8 @@
+var json_Meancoordinates_2 = {
+"type": "FeatureCollection",
+"name": "Meancoordinates_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "MEAN_X": -84.688577932222572, "MEAN_Y": 38.74399186857525 }, "geometry": { "type": "Point", "coordinates": [ -84.688577932222572, 38.74399186857525 ] } }
+]
+}
