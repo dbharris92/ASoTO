cf64bb55f4179aef7094b81ba85be45134ddb0c1
Drew Harris <harris.db92@gmail.com>
2021-06-01 14:37:03 -0400

    3 moves
    thats it

--- data/Meancoordinates_2.js
+++
@@ -1,8 +0,0 @@
-var json_Meancoordinates_2 = {
-"type": "FeatureCollection",
-"name": "Meancoordinates_2",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "MEAN_X": -84.043640937791821, "MEAN_Y": 39.183113993955914 }, "geometry": { "type": "Point", "coordinates": [ -84.043640937791821, 39.183113993955914 ] } }
-]
-}
