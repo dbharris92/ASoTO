e2e74de15231d32642196a685c73b410b3c4e25b
dbharris92 <harris.db92@gmail.com>
2023-12-11 14:08:13 -0500

    Update centroid_2.js

--- data/centroid_2.js
+++ data/centroid_2.js
@@ -3,6 +3,6 @@ var json_centroid_2 = {
 "name": "centroid_2",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "fid": "1", "MEAN_X": -84.064637757993751, "MEAN_Y": 39.225417374345504 }, "geometry": { "type": "Point", "coordinates": [  -82.88491015151034, 39.401261306697393 ] } }
+{ "type": "Feature", "properties": { "fid": "1", "MEAN_X": -83.004981957048798, "MEAN_Y": 39.382071946153225 }, "geometry": { "type": "Point", "coordinates": [  -83.004981957048798, 39.382071946153225 ] } }
 ]
 }
